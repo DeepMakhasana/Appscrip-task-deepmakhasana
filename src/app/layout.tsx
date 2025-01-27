70bc6712fe116dec582bc2bf3b86e56c81eb4445
DeepMakhasana <deepmakhasana1013@gmail.com>
2025-01-27 12:44:45 +0530

    initial setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,12 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
+const simplonNorm = localFont({
+  src: "./fonts/SimplonNorm-Regular-WebS.woff",
+  variable: "--font-simplon-norm",
   weight: "100 900",
 });
 
@@ -25,8 +22,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${simplonNorm.variable}`}>
+        <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
