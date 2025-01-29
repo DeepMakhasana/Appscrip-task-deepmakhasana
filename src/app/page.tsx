b4afc72cb7858776e8d984b2bcd1bd3c6fc02dfa
DeepMakhasana <deepmakhasana1013@gmail.com>
2025-01-29 10:37:39 +0530

    SEO metadata add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,13 @@
 import ProductFilter from "@/components/filter/ProductFilter";
 import PageHeading from "@/components/page-heading/PageHeading";
 import { IProduct } from "@/types/product";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Shop | Storefy",
+  description:
+    "Shop the latest trends and best deals on all product at Storefy. Enjoy secure shopping, fast delivery, and exclusive discounts. Find high-quality products, unbeatable prices, and exceptional customer service all in one place. Shop now and experience hassle-free online shopping!",
+};
 
 export default async function Home({
   searchParams,
